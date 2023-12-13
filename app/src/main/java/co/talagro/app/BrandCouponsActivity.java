7b41555164c440add6b9c27efc78195f1ec6f282
sheetalj2205 <sheetalj2205@gmail.com>
2023-12-14 02:43:27 +0530

    setting text for each brand card

--- app/src/main/java/co/talagro/app/BrandCouponsActivity.java
+++ app/src/main/java/co/talagro/app/BrandCouponsActivity.java
@@ -2,10 +2,12 @@ package co.talagro.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,8 +22,24 @@ public class BrandCouponsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brand_coupons);
 
-        CardView cardView = findViewById(R.id.card);
+        CardView amazonCardView = findViewById(R.id.amazon_card);
+        CardView nikeCardView = findViewById(R.id.nike_card);
+        CardView walmartCardView = findViewById(R.id.walmart_card);
 
+        createTextForAmazon();
+        createTextForNike();
+        createTextForWalmart();
+        redeemCard(amazonCardView);
+        redeemCard(nikeCardView);
+        redeemCard(walmartCardView);
+    }
+
+    private void showCardRedeems() {
+        Toast.makeText(this, "Card Reedemed!", Toast.LENGTH_SHORT).show();
+        // Add your logic here to display the card redeems
+    }
+
+    private void redeemCard(CardView cardView) {
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,9 +51,25 @@ public class BrandCouponsActivity extends AppCompatActivity {
         });
     }
 
-    private void showCardRedeems() {
-        Toast.makeText(this, "Card Reedemed!", Toast.LENGTH_SHORT).show();
-        // Add your logic here to display the card redeems
+    private void createTextForAmazon() {
+        TextView brandCouponText = findViewById(R.id.amazonCouponDescriptionTextView);
+        String htmlText = "<b>Amazon</b><br/><span>15% Off</span><br/><span>On purchase of $199 and more</span>";
+        brandCouponText.setText(Html.fromHtml(htmlText));
+
+    }
+
+    private void createTextForWalmart() {
+        TextView brandCouponText = findViewById(R.id.amazonCouponDescriptionTextView);
+        String htmlText = "<b>First line in bold</b><br/><small>Second line in smaller font</small>";
+        brandCouponText.setText(Html.fromHtml(htmlText));
+
+    }
+
+    private void createTextForNike() {
+        TextView brandCouponText = findViewById(R.id.amazonCouponDescriptionTextView);
+        String htmlText = "<b>First line in bold</b><br/><small>Second line in smaller font</small>";
+        brandCouponText.setText(Html.fromHtml(htmlText));
+
     }
 
 }
