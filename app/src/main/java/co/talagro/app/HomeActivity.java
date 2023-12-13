f585129377d21b3b2b1b5e23417fc94d5dda2e48
Jainendra Kumar <jai@tala.co>
2023-12-13 17:52:41 +0530

    Add todo for state transitions

--- app/src/main/java/co/talagro/app/HomeActivity.java
+++ app/src/main/java/co/talagro/app/HomeActivity.java
@@ -3,8 +3,11 @@ package co.talagro.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         getSupportActionBar().hide();
+
+//        if (state = application) {
+            ((TextView)findViewById(R.id.loan_card_text_heading)).setText(R.string.string_application);
+//            setApplicationText();
+//        }
+
+        // APPLICATION -> DISBURSEMENT -> REPAYMENT -> POPUP -> APPLICATION
+
+
+//        switch (currentState) {
+//            case APPLICATION:
+                //1 Update Header text
+                //2 Update body text
+                //3 Update icon
+                //4 State transition
     }
+
 }
---
+++ app/src/main/java/co/talagro/app/StateTransitionManager.java
@@ -0,0 +1,17 @@
+package co.talagro.app;
+
+public class StateTransitionManager {
+
+    static int currentState;
+
+//    public int getCurrentState() {
+//        return currentState;
+//        //TODO; persist these values
+//    }
+//
+//    public void updateCurrentState(state) {
+//        currentState = state;
+//        //TODO; persist these values
+//    }
+
+}
